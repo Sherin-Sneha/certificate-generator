cbc73c27eafc60a3ff0267c74ed7753e051447aa
Sherin Sneha J <67852150+Sherin-Sneha@users.noreply.github.com>
2021-06-01 20:27:38 +0530

    changes done for vs code

--- mainpy/main.py
+++ mainpy/main.py
@@ -1,7 +1,7 @@
 from PIL import Image
 from pandas import pandas as pd
-df = pd.read_csv('example.csv')
+df = pd.read_csv(r"C:\Users\Sherin Sneha\Documents\GitHub\certificate-generator\mainpy\example.csv")
 for index, j in df.iterrows():
-  image1 = Image.open('Certificate-png/images/imi{}.png'.format(j['ID']))
+  image1 = Image.open(r"C:\Users\Sherin Sneha\Documents\GitHub\certificate-generator\mainpy\Certificate-png\images\imi{}.png".format(j['ID']))
   im1 = image1.convert('RGB')
-  im1.save('Certificate-PDF/{}.pdf'.format(j['Name']))
+  im1.save(r"C:\Users\Sherin Sneha\Documents\GitHub\certificate-generator\mainpy\Certificates-PDF/{}.pdf".format(j['Name']))
